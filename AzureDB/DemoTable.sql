a504c1a12efff3956109cd5b25bcd688f66715bd
Sneha Vasanthakumar <s.vasanthakumar@avanade.com>
2019-01-18 17:32:36 +0100

    AureDeploymentdemo

---
+++ AzureDB/DemoTable.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[DemoTable]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Email] NCHAR(10) NULL, 
+    [Name] NCHAR(10) NULL, 
+    [Address] NCHAR(10) NULL
+)
---
+++ DemoApplication1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DemoApplication1.WebApiApplication" Language="C#" %>
